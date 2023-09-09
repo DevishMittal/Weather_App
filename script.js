e70beb5b92a1db7c5e1f81576fcef33bffc6c471
Devish Mittal <devishmittal1@gmail.com>
2023-09-10 03:11:53 +0530

    bg

--- script.js
+++ script.js
@@ -22,33 +22,33 @@ function searchWeather(searchTerm){
     function init(resultFromServer){
         switch (resultFromServer.weather[0].main){
             case "Clear":
-                document.body.style.backgroundImage = 'url("clear.jpg")';
+                document.body.style.backgroundImage = 'url("./photos/clear.jpg")';
                 break;
 
             case "Clouds":
-                document.body.style.backgroundImage = 'url("cloudy.jpg")';
+                document.body.style.backgroundImage = 'url("./photos/cloudy.jpg")';
 
             break;
 
             case "Rain":
             case "Drizzle":
-                document.body.style.backgroundImage = 'url("rain.jpg")';
+                document.body.style.backgroundImage = 'url("./photos/rain.jpg")';
         
 
             break;
 
             case "Thunderstorm":
-                document.body.style.backgroundImage = 'url("storm.jpg")';
+                document.body.style.backgroundImage = 'url("./photos/storm.jpg")';
 
             break;
             
             case "Snow":
-                document.body.style.backgroundImage = 'url("snow.jpg")';
+                document.body.style.backgroundImage = 'url("./photos/snow.jpg")';
 
             break;
 
             case "Mist":
-                document.body.style.backgroundImage = 'url("mist.jpg")';
+                document.body.style.backgroundImage = 'url("./photos/mist.jpg")';
 
             break;
 
