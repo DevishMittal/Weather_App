6190f129e4a11b18699b49ec1317c73123c07905
Devish Mittal <devishmittal1@gmail.com>
2023-09-09 18:05:47 +0530

    result bar updated

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-let appId = "cba7a255ceadaa690d0b878dca523e08";
-let units = "Metrics";
-let Search_Method = 'zip';
+let appId = 'cba7a255ceadaa690d0b878dca523e08';
+let units = 'metric';
+let searchMethod;
 
-function Search_Weather(SearchTerm){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?${Search_Method}=${SearchTerm}&APPID=$(appId)&units=$(units)`).then(result => {
+function getSearchMethod(searchTerm){
+    if (searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+        searchMethod = 'zip';
+    else 
+        searchMethod = 'q';
+}
+
+function searchWeather(searchTerm){
+    getSearchMethod(searchTerm);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
         return result.json();
 
     }).then(result =>  {
         init(result);
         
     })
+}
     function init(resultFromServer){
         console.log(resultFromServer);
         
     }
-    document.getElementById('Search_Button')
+    document.getElementById('searchBtn').addEventListener('click', () => {
+        let searchTerm = document.getElementById('searchInput').value;
+        if(searchTerm)
+            searchWeather(searchTerm);
+    })
 
-}
